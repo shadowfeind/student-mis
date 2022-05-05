19f3f49015640fecbbbe4c4765c2e4f8f39e148c
amir0123 <amirkarmacharya4@gmail.com>
2022-05-05 14:54:59 +0545

    done

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
 // export const API_URL = "http://103.90.86.151:84";
 // export const API_URL = "http://103.90.86.151:100";
 // export const API_URL = "http://192.168.1.85:88";
-export const API_URL = "https://vidyacube.com";
+export const API_URL = "https://mis.vidyacube.com";
 
 export const USER_SESSION = sessionStorage.getItem("blueberrytoken");
 
 export const tokenConfig = {
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4OTYxMjM4MS1iYzcwLTRmOWUtOGJlMC1iNmJhOTZlZTFlM2IiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTUwNjciLCJJRFVzZXIiOiIxNTA2NyIsIklEUm9sZSI6IjgiLCJNYXJrQXNBZG1pbiI6IkZhbHNlIiwiSURIUkJyYW5jaCI6IjEiLCJJRERlcGFydG1lbnQiOiIxIiwiY29tcGFueSI6IjIiLCJJc1RlbXBvcmFyeVNlc3Npb25FbmFibGVkIjoiRmFsc2UiLCJJc05ld2x5QWRlZCI6IkZhbHNlIiwiSXNEZXBhcnRtZW50SGVhZCI6IkZhbHNlIiwiUmVtZW1iZXJNZSI6IkZhbHNlIiwiRnVsbE5hbWUiOiJsNCAgbDQiLCJwaWRSZWZGb3JFZGl0IjoiNzAwMSIsImV4cCI6MTY1MTI5Nzk4NiwiaXNzIjoiaHR0cDovL215c2l0ZS5jb20iLCJhdWQiOiJodHRwOi8vbXlzaXRlLmNvbSJ9.luqpZRwxGo1CM3gJLU-D3VGX_h416V_e6B2vlejY4IQ`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwZjk4YjRkOS1iOTIzLTRmNzAtOGY3ZC1hOTQ4YjFjOGJjYjYiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTUwNjciLCJJRFVzZXIiOiIxNTA2NyIsIklEUm9sZSI6IjgiLCJNYXJrQXNBZG1pbiI6IkZhbHNlIiwiSURIUkJyYW5jaCI6IjEiLCJJRERlcGFydG1lbnQiOiIxIiwiY29tcGFueSI6IjIiLCJJc1RlbXBvcmFyeVNlc3Npb25FbmFibGVkIjoiRmFsc2UiLCJJc05ld2x5QWRlZCI6IkZhbHNlIiwiSXNEZXBhcnRtZW50SGVhZCI6IkZhbHNlIiwiUmVtZW1iZXJNZSI6IkZhbHNlIiwiRnVsbE5hbWUiOiJsNCAgbDQiLCJwaWRSZWZGb3JFZGl0IjoiTDQiLCJleHAiOjE2NTIzMzE2MTMsImlzcyI6Imh0dHA6Ly9teXNpdGUuY29tIiwiYXVkIjoiaHR0cDovL215c2l0ZS5jb20ifQ.0pgS4b2VrBDYFhLlul2CNEIsXej03RUUUthG5GcCTZ8`,
 
   },
 };
