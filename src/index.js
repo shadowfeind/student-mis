2278720699380e07a59914eb006be32e84d33cfe
amir0123 <amirkarmacharya4@gmail.com>
2022-05-20 15:18:21 +0545

    done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 
-
 // sessionStorage.setItem(
 //   "blueberrytoken",
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1NjkyZWUwMy03YzYwLTRjNTktYTlkZS0xYjQ4YjRmZDY2NDMiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTUwNzAiLCJJRFVzZXIiOiIxNTA3MCIsIklEUm9sZSI6IjgiLCJNYXJrQXNBZG1pbiI6IkZhbHNlIiwiSURIUkJyYW5jaCI6IjEiLCJJRERlcGFydG1lbnQiOiIxIiwiY29tcGFueSI6IjIiLCJJc1RlbXBvcmFyeVNlc3Npb25FbmFibGVkIjoiRmFsc2UiLCJJc05ld2x5QWRlZCI6IkZhbHNlIiwiSXNEZXBhcnRtZW50SGVhZCI6IkZhbHNlIiwiUmVtZW1iZXJNZSI6IkZhbHNlIiwiRnVsbE5hbWUiOiJPcmFuZ2UgICBCYWxsIiwicGlkUmVmRm9yRWRpdCI6Ik8xMiIsImV4cCI6MTY1MzU1MDc5MSwiaXNzIjoiaHR0cDovL215c2l0ZS5jb20iLCJhdWQiOiJodHRwOi8vbXlzaXRlLmNvbSJ9.S6pm3XTNG4mOmt1b9WZZvj6OdXcrWF1K3DoxQA5gBqc"
@@ -14,7 +13,6 @@ import App from "./App";
 //   "blueberryrefreshtoken",
 //   "lyE1UmvUkvrftApyPQ93hZIfAvP4nhPjVpneDWgS5K4="
 // );
-
 
 ReactDOM.render(
   <Provider store={store}>
